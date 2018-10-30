2cd14c3bb27976daf7de538abd1443f9be7cfee8
JFumba <jessefumba@gmail.com>
2018-10-30 10:02:46 +0100

    ish

--- src/App.js
+++ src/App.js
@@ -2,35 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import data from './movie.json'; 
 import './App.css';
- 
-class App extends Component {
-  render() {
-    return (<div className="App">
-      <div class="search">
-        
-          
-          {
-            data.movie.map(function (film){
-              return (
-                        <div class="wrapper-card">
-                        <img src={film.Poster}></img>
-                        <h3>{film.Title}</h3>
-                        <h4>{film.Year}</h4>
-                        <p>{film.Plot}</p>
-                        </div>
-                    );
-            })
-          }
-          
-        
-      </div>
-      </div>
-    );
-  }
-}
 
-export default App;
-/*class App extends React.Component {
+class App extends React.Component {
  
   render()  {
     return  (
@@ -41,18 +14,14 @@ export default App;
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
+              <Link to="/movies">Movies</Link>
             </li>
           </ul>
  
           <hr />
           <div className="main-route-place">
             <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />
+            <Route path="/movies" component={Movies} />
           </div>
         </div>
       </BrowserRouter>
@@ -72,51 +41,26 @@ class Home extends React.Component {
   }
 }
  
-class About  extends React.Component {
+class Movies  extends React.Component {
   render() {
     return (
-      <div>
-        <h2>About</h2>
-      </div>
-    );
-  }
-}
- 
-class Topics extends React.Component {
-  render( ) {
-    return (
-      <div>
-        <h2>Topics</h2>
-        <ul>
-          <li>
-            <Link to={`${this.props.match.url}/rendering`}>
-              Rendering with React
-            </Link>
-          </li>
-          <li>
-            <Link to={`${this.props.match.url}/components`}>Components</Link>
-          </li>
-          <li>
-            <Link to={`${this.props.match.url}/props-v-state`}>
-              Props v. State
-            </Link>
-          </li>
-        </ul>
- 
-        <div className="secondary-route-place">
-          <Route
-            path={`${this.props.match.url}/:topicId`}
-            component={Topic} />
-          <Route
-            exact
-            path={this.props.match.url}
-            render={() =>
-              <h3>
-                Please select a topic.
-              </h3>
-            }
-            />
-        </div>
+      <div class="search">
+        
+          
+          {
+            data.movie.map(function (film){
+              return (
+                        <div class="wrapper-card">
+                        <img src={film.Poster}></img>
+                        <h3>{film.Title}</h3>
+                        <h4>{film.Year}</h4>
+                        <p>{film.Plot}</p>
+                        </div>
+                    );
+            })
+          }
+          
+        
       </div>
     );
   }
@@ -133,5 +77,4 @@ class Topic extends React.Component {
     );
   }
 }
- 
-export default App;*/
+export default App;
